bdd731aba2960996ccf52f438449dd5fea07077d
Oetgin <oetgin@gmail.com>
2021-06-09 22:48:02 +0200

    Commands changes

--- Cogs/invite.py
+++ Cogs/invite.py
@@ -2,21 +2,31 @@
 # Copyright (C) 2021  Oetgin
 
 from discord.ext import commands
+import json
 
 
 class InviteCog(commands.Cog, name="invite command"):
 	def __init__(self, bot:commands.bot):
 		self.bot = bot
-        
+
 	@commands.command(name = "invite",
 					usage="",
 					description = "Create an invite link for the current server.")
 	@commands.cooldown(1, 2, commands.BucketType.member)
 	async def invite(self, ctx):
-        # Creating invite link
-         invitelink = await ctx.channel.create_invite()
-        # Dming it to the person 
-         await ctx.send("Do you want to invite comrades to your server ? Use this link : " + str(invitelink))
+		with open("Cogs/commands_config.json", "r") as config:
+			data = json.load(config)
+			quote_config = data["invite"]
+			if quote_config[0] == "0":
+				DeleteMessage = False
+			else:
+				DeleteMessage = True
+		# Creating invite link
+		invitelink = await ctx.channel.create_invite()
+		# Dming it to the person 
+		await ctx.send("Do you want to invite comrades to your server ? Use this link : " + str(invitelink))
+		if DeleteMessage:
+			await ctx.message.delete()
 
 def setup(bot:commands.Bot):
 	bot.add_cog(InviteCog(bot))
